afd6b4ae69f2957758d46afde9f6bffd4636cddb
Shayaan Ahmed Farooqi <fleshlime.1@gmail.com>
2021-03-02 01:12:20 +0500

    Followed The First Intro to EfCore 5 Video and implemented that in this.

--- using-external-apis/Controllers/WeatherForecastController.cs
+++ using-external-apis/Controllers/WeatherForecastController.cs
@@ -4,36 +4,110 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Google.Apis.Gmail.v1.Data;
+using Google.Apis.Gmail.v1;
+
+
+using Google.Apis.Discovery.v1;
+using Google.Apis.Discovery.v1.Data;
+using Google.Apis.Services;
+using System.ComponentModel.DataAnnotations;
+
+using Microsoft.EntityFrameworkCore;
 
 namespace using_external_apis.Controllers
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+  [ApiController]
+  [Route("[controller]")]
+  public class WeatherForecastController : ControllerBase
+  {
+    private static readonly string[] Summaries = new[]
     {
-        private static readonly string[] Summaries = new[]
-        {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+    private readonly ILogger<WeatherForecastController> _logger;
+    private readonly CookbookContext _context;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
-
-        [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
+    public WeatherForecastController(ILogger<WeatherForecastController> logger, CookbookContext DataContext)
+    {
+      _logger = logger;
+      _context = DataContext;
     }
+
+    [HttpGet]
+    public async Task<List<Dish>> GetAll()
+    {
+      return await _context.Dishes.ToListAsync();
+    }
+
+    [HttpGet("gmail")]
+    public async Task<Profile> GetGmail()
+    {
+      var service = new DiscoveryService(new BaseClientService.Initializer
+      {
+        ApplicationName = "IDK lol",
+        ApiKey = "AIzaSyAafDI3EmFtj3W3VskJILLum_IJ16RZZ24"
+      });
+
+      var gmailService = new GmailService(new BaseClientService.Initializer
+      {
+        ApplicationName = "my Gmail Service",
+        ApiKey = "AIzaSyAafDI3EmFtj3W3VskJILLum_IJ16RZZ24",
+      });
+
+      var response = await service.Apis.List().ExecuteAsync();
+      var gmailResponse = await gmailService.Users.GetProfile("me").ExecuteAsync();
+      if (response.Items != null)
+      {
+        foreach (DirectoryList.ItemsData api in response.Items)
+        {
+          Console.WriteLine(api.Id + " - " + api.Title);
+        }
+      }
+
+      return gmailResponse;
+    }
+  }
+
+  public class Dish
+  {
+    public int Id { get; set; }
+
+    [MaxLength(100)]
+    public string Title { get; set; } = string.Empty;
+
+    public string? Notes { get; set; }
+    public int? Stars { get; set; }
+    public List<DishIngredient> Ingredients { get; set; } = new List<DishIngredient>();
+  }
+
+  public class DishIngredient
+  {
+    public int Id { get; set; }
+
+    [MaxLength(100)]
+    public string Description { get; set; } = string.Empty;
+
+    [MaxLength(50)]
+    public string UnitOfMeasure { get; set; } = string.Empty;
+
+    // [Column(TypeName = "decimal(5, 2)")]
+    public decimal Amount { get; set; }
+
+    public int DishId { get; set; }
+  }
+
+  public class CookbookContext : DbContext
+  {
+    public DbSet<Dish> Dishes { get; set; }
+    public DbSet<DishIngredient> Ingredients { get; set; }
+
+#pragma warning disable CS8618
+    public CookbookContext(DbContextOptions<CookbookContext> options)
+    : base(options)
+    {
+
+    }
+  }
 }
--- using-external-apis/README.md
+++ using-external-apis/README.md
@@ -1,3 +1,52 @@
 # Using external Apis
 
 The idea is to understand how to use third party apis. Like in python I can use `requests` or in javascript I can use `fetch` or `axios` or some client library of the api provider.
+
+## Entity Framework
+
+### Prerequisites
+
+- `dotnet tool update --global dotnet-ef --version 3.1.12`
+- Have some database server running in the background. I'm using Postgres inside docker on localhost.
+- Add the Nuget Packages to `.csproj`
+  ```
+    <PackageReference Include="Npgsql.EntityFrameworkCore.PostgreSQL" Version="3.1.11"/>
+    <PackageReference Include="Microsoft.EntityFrameworkCore.Design" Version="3.1.12"/>
+  ```
+- Add the Model classes.
+- Create DbContext where Tables are represented as DbSets by using the Model Classes.
+
+  ```csharp
+    public class CookbookContext : DbContext
+    {
+      public DbSet<Dish> Dishes { get; set; }
+      public DbSet<DishIngredient> Ingredients { get; set; }
+
+    #pragma warning disable CS8618
+      public CookbookContext(DbContextOptions<CookbookContext> options)
+      : base(options)
+      {
+
+      }
+    }
+  ```
+
+- Register the DbContext with Dependency Injection inside `Startup.cs`
+  ```csharp
+    services.AddDbContext<CookbookContext>(options =>
+        options.
+        UseLoggerFactory(LoggerFactory.Create(builder => builder.AddConsole())) // prints sql queries to console.
+        .UseNpgsql(Configuration.GetConnectionString("CookbookContext")));
+  ```
+- Add the ConnectionString to the `appsettings.Development.json`
+  ```json
+  {
+    "ConnectionStrings": {
+      "CookbookContext": "host=localhost;database=postgres;user id=postgres;password=docker;"
+    }
+  }
+  ```
+- Use the DbContext inside whatever controllers etc.
+- Actually generate the code to create the tables by using `dotnet ef migrations add initial`.
+  - Can also use `dotnet ef migrations script` to generate the SQL script version.
+- Write the migration to the database with `dotnet ef database update`.
